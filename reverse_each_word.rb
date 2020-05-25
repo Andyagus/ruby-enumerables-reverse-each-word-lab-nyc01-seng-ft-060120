273cd66d1f3701b970cdecd57c8fdba47dd20d60
Andy Agus <redagus@gmail.com>
2020-05-25 21:44:23 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,16 +1,3 @@
-
-
-
-def reverse_each_word(sentence)
-  words_arr = sentence.split(" ")
-  new_array = []
-  new_array << words_arr.collect{|letter| letter.reverse}
-  new_array.join(" ")
-end
-
-
-
-
 def reverse_each_word(sentance)
   words_arr = sentance.split(" ")
   new_arr = []
